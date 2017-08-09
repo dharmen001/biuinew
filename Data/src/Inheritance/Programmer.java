7f5b6eaee1706accd70ddd971c2ea554f818ca8f
dharmen001 <001.dharmendra@gmail.com>
2017-08-09 11:23:48 +0530

    Inheritance

---
+++ Data/src/Inheritance/Programmer.java
@@ -0,0 +1,20 @@
+package Inheritance;
+
+public class Programmer extends Employee{
+    int bonus = 1000;
+
+    public static void main(String[] args) {
+        Programmer p = new Programmer();
+
+        System.out.println("Programmer salary is"+" "+ p.bonus);
+        System.out.println("Employee salary is"+" "+p.salary);
+
+
+    }
+
+}
+
+class Employee{
+
+    float salary = 10000;
+}
