cbd00aeede3a44295ec7d3affb3013872886c8ed
dharmendra mishra <dharmendra.mishra@exponential.com>
2017-08-11 16:45:16 +0530

    Hoerarichalinheritance

---
+++ Data/src/Inheritance/HierarichalInheritance/HierarichalInheritance.java
@@ -0,0 +1,39 @@
+package Inheritance.HierarichalInheritance;
+
+public class HierarichalInheritance {
+
+    public static void main(String[] args) {
+
+        cat c = new cat();
+        c.Meow();
+        c.eat();
+        //c.barking();//CT Error
+
+
+    }
+
+}
+
+class Animal{
+
+    void eat(){
+
+        System.out.println("eating..");
+    }
+        }
+
+class Dog extends Animal{
+
+    void barking(){
+
+        System.out.println("Barking");
+    }
+}
+
+class cat extends Animal{
+
+    void Meow(){
+
+        System.out.println("Meow..");
+    }
+}
